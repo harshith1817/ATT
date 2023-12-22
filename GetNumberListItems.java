fbc14d3380c4e382a741b36cd7f131aa556a0660
HARSHITH CHINTAKINDI <127752210+harshith1817@users.noreply.github.com>
2023-12-23 00:02:18 +0530

    Week9

--- GetNumberListItems.java
+++ GetNumberListItems.java
@@ -1,10 +1,10 @@
-package arjun;
+package Harshith;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class week9 {
+public class Week9 {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\chint\\Downloads\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
