0c952f247516632840359b391c6c62b951635fd9
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2023-12-31 11:08:13 +0530

    Week7

--- GoogleSearchCmrit.java
+++ GoogleSearchCmrit.java
@@ -1,37 +1,22 @@
-package Harshith;
+package att;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Week7 {
     public static void main(String[] args) {
-        // Set the path to the ChromeDriver executable (update this path according to your system)
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chint\\Downloads\\chromedriver.exe");
-
-        // Create a new instance of the ChromeDriver
+        System.setProperty("webdriver.chromedriver", "C:\\Users\\chint\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-
-        // Navigate to Google
+		driver.manage().window().maximize();
         driver.get("https://www.google.com");
-
-        // Find the search input element
         WebElement searchBox = driver.findElement(By.name("q"));
-
-        // Enter "CMRIT" into the search input
         searchBox.sendKeys("CMRIT");
-
-        // Submit the search form
         searchBox.submit();
-
-        // Wait for a few seconds (you can use explicit waits for better handling)
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        // Close the browser
         driver.quit();
     }
 }
