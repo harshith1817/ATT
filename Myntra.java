a20930955c72cc46ce464e57cc6aaad2d7f1feb1
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2024-01-04 00:14:35 +0530

    Week11

--- Myntra.java
+++ Myntra.java
@@ -1,4 +1,4 @@
-package Harshith;
+package att;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,18 +13,16 @@ public class Week11 {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.myntra.com/");
-		
-		WebElement Login = driver.findElement(By.className("desktop-userTitle"));
-		new Actions(driver).moveToElement(Login).perform();
+		Actions action=new Actions(driver);
+		WebElement Login = driver.findElement(By.xpath("/html/body/div[1]/div/div/header/div[2]/div[2]/div/div[1]/span[1]"));
+		action.moveToElement(Login).perform();
 		Thread.sleep(2000);
 		WebElement registerBtn = driver.findElement(By.className("desktop-linkButton"));
 		registerBtn.click();
-		
-		WebElement phone_number = driver.findElement(By.className("mobileNumberInput"));
+		WebElement phone_number = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/div[2]/div/div/div[1]/div/div/div[2]/div[2]/div[1]/input"));
 		phone_number.click();
 		phone_number.sendKeys("8019801084");
-		
-		WebElement continueBtn = driver.findElement(By.className("submitBottomOption"));
+		WebElement continueBtn = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/div[2]/div/div/div[1]/div/div/div[2]/div[2]/div[3]"));
 		continueBtn.click();
 		
 	}
